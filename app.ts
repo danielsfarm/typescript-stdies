7528ea47e730fd513a4f8156b949041ae40e4429
Daniel Selga <danielselga@gmail.com>
2022-02-07 12:05:31 -0300

    chore: never type

--- app.ts
+++ app.ts
@@ -6,3 +6,9 @@ userInput = 'Max'
 if (typeof userInput === 'string') { // To asign unknow we need extra type check
     userName = userInput
 }
+
+function generateError(message: string, code: number): never { // Never explicits say this function never return anything
+    throw {message: message, errorCode: code}
+}
+
+generateError('An Error ocurred!', 500)
